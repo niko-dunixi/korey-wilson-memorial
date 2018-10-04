405d03fea44c368a05a40adff83047aff201e314
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-10-03 20:50:13 -0600

    Playlist functionality works, but only because I'm doing it myself.

--- korey-wilson-memorial-app/src/app/background-music-player/background-music-player.component.ts
+++ korey-wilson-memorial-app/src/app/background-music-player/background-music-player.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackgroundMusicPlayerComponent implements OnInit {
 
-  constructor() { }
+  songs: string[];
+  songIndex = 0;
+
+  constructor() {
+    const folder = "/assets/audio/";
+    this.songs = [
+      folder + "01 Rest in Peace.m4a",
+      folder + "02 What Appears.m4a",
+      folder + "03 Got Yours.m4a",
+      folder + "04 A Place We Set Afire.m4a",
+      folder + "05 Leave a Light On.m4a",
+      folder + "06 The Hurt Is Gone.m4a",
+      folder + "07 Empty Street.m4a",
+      folder + "08 I'm a Wrecking Ball.m4a",
+      folder + "09 Savior's Robes.m4a",
+      folder + "10 Fields & Fences.m4a",
+    ];
+   }
 
   ngOnInit() {
+  }
+
+  advancePlayerTrack() {
+    this.songIndex = (this.songIndex + 1) % this.songs.length;
   }
 
 }
