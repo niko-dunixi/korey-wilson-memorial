0b4a6e4274b0af1c439b7935f9ba4008ea6f3dcc
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-10-03 18:03:35 -0600

    Adding two seconds to every photo

--- korey-wilson-memorial-app/src/app/photo-slideshow/photo-slideshow.component.ts
+++ korey-wilson-memorial-app/src/app/photo-slideshow/photo-slideshow.component.ts
@@ -38,7 +38,7 @@ export class PhotoSlideshowComponent implements OnInit {
     this.maxImageCount = 4;
     this.selectRandomIndex();
     this.setImage();
-    this.looper = timer(1000, 10000);
+    this.looper = timer(1000, 12000);
   }
 
   ngOnInit() {
@@ -51,7 +51,7 @@ export class PhotoSlideshowComponent implements OnInit {
         this.setImage();
         setTimeout(() => {
           this.toggleVisible();
-        }, 8500);
+        }, 10500);
       },1500);
     });
   }
